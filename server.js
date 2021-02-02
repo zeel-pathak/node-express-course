1fb0582fa6b83980627c6264565168f8197695ae
Zeel Pathak <zeelpathak2018.it@gmail.com>
2021-02-02 23:52:03 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.get('/users',function(req,res){
     })
 })
 
+app.get('/users/:id',function(req,res){
+    res.json({
+        success : true,
+        message : 'successFully got users. Nice!',
+        users : req.params.id
+    })
+})
+
 app.listen(8000,function(){
     console.log("server is running")
 })
